299cb83bc92d8c7e23106ae904bd202d0ff57741
Electrk <Electrk@ymail.com>
2019-07-14 19:22:56 -0700

    Added type asserts and checks for non-number app/PxSys versions

--- classes/PxSysServerInfo/PxSysServerInfo.js
+++ classes/PxSysServerInfo/PxSysServerInfo.js
@@ -4,6 +4,7 @@ const has = require ('has-own-property-x');
 const EnumBag   = rfr ('classes/EnumBag.js');
 const ErrorList = rfr ('classes/ErrorList.js');
 
+const { numberTypeAssert, stringTypeAssert } = rfr ('utility/typeAssert.js');
 const { requiredArgsAssert } = rfr ('utility/miscellaneous.js');
 const { PXSYS_VERSION }      = rfr ('miscellaneous/constants.js');
 
@@ -18,6 +19,11 @@ class PxSysServerInfo
 		const { loginKey, adminKey, appVersion, appName } = info;
 
 		requiredArgsAssert ({ loginKey, adminKey, appVersion, appName });
+
+		stringTypeAssert (loginKey, 'loginKey');
+		stringTypeAssert (adminKey, 'adminKey');
+		stringTypeAssert (appName, 'appName');
+		numberTypeAssert (appVersion, 'appVersion');
 
 		this.appVersion = appVersion;
 		this.appName    = appName;
@@ -81,7 +87,11 @@ class PxSysServerInfo
 				errors.push ('CL_BAD_ADMIN');
 			}
 
-			if ( pxSysVersion < PXSYS_VERSION )
+			if ( isNaN (pxSysVersion) )
+			{
+				errors.push ('CL_BAD_PX_VER');
+			}
+			else if ( pxSysVersion < PXSYS_VERSION )
 			{
 				errors.push ('CS_PX_VER_NEW');
 			}
@@ -90,7 +100,11 @@ class PxSysServerInfo
 				errors.push ('CS_PX_VER_OLD');
 			}
 
-			if ( appVersion < this.appVersion )
+			if ( isNaN (appVersion) )
+			{
+				errors.push ('CL_BAD_APP_VER');
+			}
+			else if ( appVersion < this.appVersion )
 			{
 				errors.push ('CS_APP_VER_NEW');
 			}
