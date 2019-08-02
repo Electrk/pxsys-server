dc1bf8759bf2f97b1880354c9d84d41df345671a
Electrk <Electrk@ymail.com>
2019-08-02 01:14:37 -0700

    Made sockets Set "private"

--- classes/PxSysServer.js
+++ classes/PxSysServer.js
@@ -9,7 +9,7 @@ const { instanceOfAssert }   = rfr ('utility/typeAssert.js');
 
 class PxSysServer
 {
-	constructor ( port, address, onServerStart = function () {}, onServerEnd = function () {} )
+	constructor ( port, address, onServerStart = () => {}, onServerEnd = () => {} )
 	{
 		requiredArgsAssert ({ port, address });
 
@@ -18,10 +18,10 @@ class PxSysServer
 		this.isDeleted = false;
 		this.port      = port;
 		this.address   = address;
-		this.sockets   = new Set ();
 
-		this._onEnd  = onServerEnd;
-		this._server = server.listen (port, address, onServerStart);
+		this._sockets = new Set ();
+		this._onEnd   = onServerEnd;
+		this._server  = server.listen (port, address, onServerStart);
 	}
 
 	delete ( callback = function () {} )
@@ -36,16 +36,16 @@ class PxSysServer
 			callback ();
 			this._onEnd ();
 
-			for ( let socket of this.sockets )
+			for ( let socket of this._sockets )
 			{
 				socket.end ();
 			}
 
-			this.sockets.clear ();
+			this._sockets.clear ();
 
 			delete this.port;
 			delete this.address;
-			delete this.sockets;
+			delete this._sockets;
 			delete this._onEnd;
 			delete this._server;
 
@@ -58,12 +58,12 @@ class PxSysServer
 	addSocket ( socket )
 	{
 		instanceOfAssert (socket, net.Socket, 'socket');
-		this.sockets.add (socket);
+		this._sockets.add (socket);
 	}
 
 	removeSocket ( socket )
 	{
-		this.sockets.delete (socket);
+		this._sockets.delete (socket);
 	}
 
 	sendCommand ( socket, command, ...args )
@@ -81,7 +81,7 @@ class PxSysServer
 			return;
 		}
 
-		for ( let socket of this.sockets )
+		for ( let socket of this._sockets )
 		{
 			this.sendCommand (socket, command, ...args);
 		}
@@ -89,18 +89,12 @@ class PxSysServer
 
 	on ( event, callback )
 	{
-		if ( !this.isDeleted )
-		{
-			return this._server.on (event, callback);
-		}
+		return this._server.on (event, callback);
 	}
 
 	off ( event, callback )
 	{
-		if ( !this.isDeleted )
-		{
-			return this._server.off (event, callback);
-		}
+		return this._server.off (event, callback);
 	}
 }
 
