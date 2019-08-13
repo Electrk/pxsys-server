87baaa768281062aae7201bc2c17f2ab359e0714
Electrk <Electrk@ymail.com>
2019-08-13 01:36:02 -0700

    Removed EnumBag -- commands and errors are now just sent as strings

--- classes/PxSys/PxSys.js
+++ classes/PxSys/PxSys.js
@@ -1,10 +1,6 @@
 const rfr = require ('rfr');
 
 const PxSysScreen = rfr ('classes/PxSysScreen.js');
-const EnumBag     = rfr ('classes/EnumBag.js');
-
-const defaultCommandCodes = require ('./defaultCommandCodes.js');
-const defaultErrorCodes   = require ('./defaultErrorCodes.js');
 
 const { requiredArgsAssert } = rfr ('utility/miscellaneous.js');
 const { instanceOfAssert }   = rfr ('utility/typeAssert.js');
@@ -19,10 +15,8 @@ class PxSys
 
 		this.isDeleted = false;
 
-		this._server       = null;
-		this._screen       = screen;
-		this._commandCodes = new EnumBag (...defaultCommandCodes);
-		this._errorCodes   = new EnumBag (...defaultErrorCodes);
+		this._server = null;
+		this._screen = screen;
 	}
 
 	delete ( onServerClose )
@@ -38,69 +32,17 @@ class PxSys
 		this._server = null;
 
 		delete this._screen;
-		delete this._commandCodes;
-		delete this._errorCodes;
 
 		this.isDeleted = true;
 	}
 
-	addCommand ( commandString )
-	{
-		if ( !this._commandCodes.has (commandString) )
-		{
-			this._commandCodes.add (commandString);
-		}
-
-		return this._commandCodes.get (commandString);
-	}
-
-	addError ( errorString )
-	{
-		if ( !this._errorCodes.has (errorString) )
-		{
-			this._errorCodes.add (errorString);
-		}
-
-		return this._errorCodes.get (errorString);
-	}
-
-	getCommandCode ( name )
-	{
-		return this._commandCodes.get (name);
-	}
-
-	getErrorCode ( name )
-	{
-		return this._errorCodes.get (name);
-	}
-
-	getCommandString ( code )
-	{
-		return this._commandCodes.getName (code);
-	}
-
-	getErrorString ( code )
-	{
-		return this._errorCodes.getName (code);
-	}
-
 	sendSocketCommand ( socket, command, ...args )
 	{
-		if ( typeof command === 'string' )
-		{
-			command = this.getCommandCode (command);
-		}
-
 		this._server.sendCommand (socket, command, ...args);
 	}
 
 	sendSocketError ( socket, errorCommand, errorCode, errorMessage )
 	{
-		if ( typeof errorCode === 'string' )
-		{
-			errorCode = this.getErrorCode (errorCode);
-		}
-
 		this.sendSocketCommand (socket, errorCommand, errorCode, errorMessage);
 	}
 
@@ -110,7 +52,7 @@ class PxSys
 
 		this._server.forEach (socket =>
 		{
-			pxObject.sendSocketCommand (socket, command ...args);
+			pxObject.sendSocketCommand (socket, command, ...args);
 		});
 	}
 
